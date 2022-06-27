50ffeddaef31a9edd5d0475fbfe995d64ff2e497
peter <peterjemiyotan@gmail.com>
2022-06-27 12:47:04 +0100

    twelvecommit

---
+++ wing.js
@@ -0,0 +1,184 @@
+let bars = document.querySelector(".hamburger i");
+let nav = document.querySelector(".menu-container");
+let home = document.querySelector(".home")
+let ticket = document.querySelector(".ticket")
+let monthly = document.querySelector(".monthly")
+let free = document.querySelector(".free")
+let vip = document.querySelector(".vip")
+let left = document.querySelector(".left-icon");
+let right = document.querySelector(".right-icon");
+
+
+bars.addEventListener("click", function(){
+    if(bars.classList.contains("fa-bars")){
+        bars.classList.add("fa-times");
+        
+        bars.classList.remove("fa-bars");
+        nav.style.height= "40px";
+        
+        
+    }else{
+        bars.classList.add("fa-bars");
+        bars.classList.remove("fa-times");
+        nav.style.height = "0";
+        
+       
+        
+    }
+})
+console.log(nav)
+
+home.addEventListener("click", ()=>{
+    home.style.color = "green";
+    home.style.border = "1px solid green"
+    ticket.style.color ="white";
+    monthly.style.color = "white";
+    free.style.color = "white"
+    vip.style.color = "white"
+})
+
+ticket.addEventListener("click", ()=>{
+    ticket.style.color = "green";
+    home.style.color = "white"
+    monthly.style.color = "white";
+    free.style.color = "white"
+    vip.style.color = "white"
+})
+
+monthly.addEventListener("click", ()=>{
+    monthly.style.color = "green";
+    ticket.style.color = "white";
+    home.style.color = "white"
+    free.style.color = "white"
+    vip.style.color = "white"
+})
+
+free.addEventListener("click", ()=>{
+    free.style.color = "green";
+    // free.style.textDecoration ="underline";
+    monthly.style.color = "white";
+    ticket.style.color = "white";
+    home.style.color = "white";
+    vip.style.color = "white";
+})
+
+vip.addEventListener("click", ()=>{
+    vip.style.color = "green"
+    free.style.color = "white"
+    monthly.style.color = "white";
+    ticket.style.color = "white";
+    home.style.color = "white"
+})
+
+
+let sliderIndex = 0;
+plusSlides();
+function plusSlides(){
+    let table = document.getElementsByClassName("slider-container")
+
+    for(let i = 0; i < table.length; i++){
+        table[i].style.display = "none";
+    }
+    sliderIndex++;
+    if(sliderIndex > table.length){sliderIndex = 1};
+    table[sliderIndex - 1].style.display = "block";
+    setTimeout(plusSlides, 10000)
+}
+
+
+left.addEventListener("click", function(){
+    let table = document.getElementsByClassName("slider-container");
+    sliderIndex += 1;
+    if(sliderIndex > table.length){
+        sliderIndex = 1
+    }
+    
+    for(let index = 0; index < table.length; index++){
+        table[index].style.display = "none";
+    }
+
+    table[sliderIndex - 1].style.display = "block";
+})
+
+right.addEventListener("click", function(){
+    let table = document.getElementsByClassName("slider-container");
+    sliderIndex -= 1;
+
+    if(sliderIndex < 1){
+        sliderIndex = table.length
+    }
+    for(let index = 0; index < table.length; index++){
+        table[index].style.display = "none";
+        table[sliderIndex - 1].style.display = "block";
+    }
+})
+
+var countDownDate = new Date("July 1, 2022 15:37:25").getTime();
+var x = setInterval(() => {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 *24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("time-out").innerHTML = days + "d" + " " + hours + "h" + " " +minutes + "m" + " " + seconds + "s";
+
+    if (distance < 0){
+        clearInterval(x);
+        document.getElementById("time-out").innerHTML = "Inplay";
+    }
+}, 1000);
+
+
+var countDownDates = new Date("July 1, 2022 15:37:25").getTime();
+var x = setInterval(() => {
+    var now = new Date().getTime();
+    var distance = countDownDates - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 *24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("time-out-one").innerHTML = days + "d" + " " + hours + "h" + " " +minutes + "m" + " " + seconds + "s";
+
+    if (distance < 0){
+        clearInterval(x);
+        document.getElementById("time-out-one").innerHTML = "Inplay";
+    }
+}, 1000);
+
+
+var countDownDatesOne = new Date("July 1, 2022 15:37:25").getTime();
+var x = setInterval(() => {
+    var now = new Date().getTime();
+    var distance = countDownDatesOne - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 *24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("time-out-two").innerHTML = days + "d" + " " + hours + "h" + " " +minutes + "m" + " " + seconds + "s";
+
+    if (distance < 0){
+        clearInterval(x);
+        document.getElementById("time-out-two").innerHTML = "Inplay";
+    }
+}, 1000);
+
+var countDownDatesTwo = new Date("July 1, 2022 15:37:25").getTime();
+var x = setInterval(() => {
+    var now = new Date().getTime();
+    var distance = countDownDatesTwo - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("time-out-three").innerHTML = days + "d" + " " + hours + "h" + " " +minutes + "m" + " " + seconds + "s";
+
+    if (distance < 0){
+        clearInterval(x);
+        document.getElementById("time-out-three").innerHTML = "Inplay";
+    }
+}, 1000);
